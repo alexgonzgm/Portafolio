82e7dc414940448498ba6f0c0b5445876267c429
Alex <alexgonzgm@gmail.com>
2020-05-03 21:46:00 +0200

    Creación de el modelo Project

---
+++ models/project.model.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var projecSchema = mongoose.Schema({
+name :String,
+description :String,
+category : String ,
+langs : String ,
+year : Number ,
+image : String
+});
+
+module.exports = mongoose.model("project",projecSchema);
