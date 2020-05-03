4b8263e1a8d7c8703133b3649c8ef4df3a814a46
Alex <alexgonzgm@gmail.com>
2020-05-03 22:27:09 +0200

    Se cargan las rutas de project.routes y express utiliza esas rutas

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+//Archivos de rutas de los distintos controladores 
+var projectRoutes = require("./routes/project.routes");
+
+//Middleware
 app.use( bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-///Exportacion del modulo
+
+//Todas las rutas 
+app.use('/api', projectRoutes)
+
+///Exportacion del modulo hacia el index.js para crear el servidor 
 module.exports = app;
 
