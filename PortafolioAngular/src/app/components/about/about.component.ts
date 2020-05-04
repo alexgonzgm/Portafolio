5d279aee3f695087f01e602e6915b97c269bbc27
Alex <alexgonzgm@gmail.com>
2020-05-04 17:39:33 +0200

    Desarrollo sobre el componente about

--- PortafolioAngular/src/app/components/about/about.component.ts
+++ PortafolioAngular/src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title : string;
+  public subtitle : string;
+  public email : string;
 
-  constructor() { }
+  constructor() {
+    this.title = "Alexander González";
+    this.subtitle = "Desarrollador Web";
+    this.email = "alexgonzgm@gmial.com";
+   }
 
   ngOnInit(): void {
   }
