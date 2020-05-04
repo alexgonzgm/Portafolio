1ed754581911e60dcf14b54fbac0ed6a45aa97ab
Alex <alexgonzgm@gmail.com>
2020-05-04 17:00:24 +0200

    Creaccion de los componentes y declaracion en app.module
     AppComponent,
    AboutComponent,
        ContactComponent,
        CreateComponent,
        DetailComponent,
        EditComponent,
        ErrorComponent,
        ProjectsComponent

--- PortafolioAngular/src/app/app.module.ts
+++ PortafolioAngular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    CreateComponent,
+    DetailComponent,
+    EditComponent,
+    ErrorComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
