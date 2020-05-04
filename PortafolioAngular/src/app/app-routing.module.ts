51c8f2975d5cd27cfd6eb8972af04fa72ffab747
Alex <alexgonzgm@gmail.com>
2020-05-04 17:13:53 +0200

    Nav-bar ,routes ,styles
    Se crea la barra de navegación con links hacia los distinos componentes que estan en el cuerpo del hatml principal 'app.component.html' ,se aplica estiloe generales de todo el proyecto y se crea la carpeta img para almacenar la foto que sera el bg principal.

--- PortafolioAngular/src/app/app-routing.module.ts
+++ PortafolioAngular/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ErrorComponent } from './components/error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'sobre-mi', component: AboutComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'crear-proyecto', component: CreateComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'proyecto/:_id', component: DetailComponent },
+  { path: 'edit/:_id', component: EditComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
