9feae52bea05024314a7fe581d3b65307a8bf6ae
Alex <alexgonzgm@gmail.com>
2020-05-04 12:46:30 +0200

    Creación de las rutas para los métodos anteriores

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -5,6 +5,10 @@ var projectController = require('../controlles/project.controller');
 
 //***** Rutas *****//
 router.post("/project",projectController.saveProject);
+router.get("/project",projectController.getProject);
+router.get("/projects",projectController.getProject);
+router.put("/project/:id",projectController.updateProject);
+router.delete("/project/:id",projectController.deleteProject);
 
 //exportamos las rutas del controlador project hacia app.js
 //donde esta la configuracion general
